67603cfc768226cbc4e85793d0e0a8fbee59503d
Helder Mauricio da Silva Cabo <heda-sil@student.42porto.com>
2024-06-04 16:43:49 +0100

    feat(mariadb): updated mariadb script
    Added user and database setup

--- src/requirements/mariadb/tools/mariadb_setup.sh
+++ src/requirements/mariadb/tools/mariadb_setup.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
-echo "Hello from mariadb"
+# Init service manager and setup mariadb
+openrc default
+rc-service mariadb setup
+
+# Start mariadb service
+rc-service mariadb start
+
+# Secure mariadb installation
+mysql_secure_installation << EOF
+
+y
+$MYSQL_ROOT_PASSWORD
+$MYSQL_ROOT_PASSWORD
+y
+y
+y
+y
+EOF
+
+# Create database and user
+mariadb -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $MYSQL_DATABASE;"
+mariadb -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mariadb -uroot -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mariadb -uroot -p$MYSQL_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+mariadb -uroot -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+
+# Stop mariadb service
+mariadb-admin -uroot -p$MYSQL_ROOT_PASSWORD shutdown
+
+# Start mariadb service
+mysqld_safe
