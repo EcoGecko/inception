a0f2feb7b3b41ffad84c2d422bd71d4a8ed92b81
Helder Mauricio da Silva Cabo <heda-sil@student.42porto.com>
2024-06-19 21:57:31 +0100

    WIP: Stuff still broken
    debian: kinda works but not really:
        - mariadb: seems to work fine;
        - nginx: seems to work fine;
        - wordpress: launch script works from inside container but not from
    		 container;
    alpine: kinda like debian but more trouble:
        - mariadb: seems to work but it crashes the db server
        - nginx: looks fine;
        - wordpress: cannot connect to database cause it crashes

--- src/requirements/mariadb/tools/mariadb_setup.sh
+++ src/requirements/mariadb/tools/mariadb_setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# Init service manager and setup mariadb
+# # Init service manager and setup mariadb
 openrc default
 rc-service mariadb setup
 
-# Start mariadb service
+# # Start mariadb service
 rc-service mariadb start
+# service mariadb start;
 
+sleep 1
 # Secure mariadb installation
 mysql_secure_installation << EOF
 
@@ -19,9 +21,11 @@ y
 y
 EOF
 
+sleep 1
+
 # Create database and user
-mariadb -uroot -p${SQL_ROOT_PASSWORD} -e "CREATE DATABASE \`${SQL_DATABASE}\`;"
-mariadb -uroot -p${SQL_ROOT_PASSWORD} -e "CREATE USER \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mariadb -uroot -p${SQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mariadb -uroot -p${SQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mariadb -uroot -p${SQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mariadb -uroot -p${SQL_ROOT_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mariadb -uroot -p${SQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
@@ -29,5 +33,7 @@ mariadb -uroot -p${SQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 # Stop mariadb service
 mariadb-admin -uroot -p${SQL_ROOT_PASSWORD} shutdown
 
+sleep 1
+
 # Start mariadb service
-mysqld_safe
+# mysqld_safe
--- src/requirements/wordpress/tools/wordpress_setup.sh
+++ src/requirements/wordpress/tools/wordpress_setup.sh
@@ -8,35 +8,42 @@ print_red() {
   echo -e "\e[31m$1\e[0m"
 }
 
-wp core download --path='/var/www/html' --allow-root
+sleep 1
 
+wp core download --path='/var/www/html' --allow-root
+print_green "downloaded wordpress"
+ 
 # SQL_DATABASE=mariadb
 # SQL_HOST=mariadb:3306
-# SQL_USER=admin
+# SQL_USER=heda-sil
 # SQL_PASSWORD=0123456789
-# SQL_ROOT_PASSWORD=0123456789
+# SQL_ROOT_PASSWORD=9876543210
 
 # # Wordpress
 # WP_URL=heda-sil.42.fr
 # WP_TITLE=no-title-page
 # WP_USER=heda-sil
 # WP_EMAIL=heda-sil@example.com
-# WP_PASSWORD=9876543210
+# WP_PASSWORD=0123456789
 # WP_ADMIN=admin
 # WP_ADMIN_EMAIL=admin@example.com
-# WP_ADMIN_PASSWORD=0123456789
+# WP_ADMIN_PASSWORD=9876543210
+
+sleep 1
 
 wp core config --dbname=$SQL_DATABASE \
   --dbuser=$WP_USER \
   --dbpass=$WP_PASSWORD \
-  --dbhost=$WP_HOST \
+  --dbhost=$SQL_HOST \
   --path='/var/www/html' \
   --allow-root
+
+sleep 1
 
 if ! wp core is-installed --path='/var/www/html' --allow-root; then
   wp core install --url=$WP_URL \
     --title=$WP_TITLE \
-    --admin_user=$WP_ADMIN_USER \
+    --admin_user=$WP_ADMIN \
     --admin_password=$WP_ADMIN_PASSWORD \
     --admin_email=$WP_ADMIN_EMAIL \
     --skip-email \
@@ -49,10 +56,12 @@ else
 	print_red "wordpress already configured"
 fi
 
+sleep 1
+
 if ! wp user list --field=user_login  --path='/var/www/html' --allow-root | grep -q "^$WP_USER$"; then
   	wp user create 	$WP_USER $WP_USER_EMAIL \
 			   		--role=author \
-					--user_pass=$WP_USER_PASSWORD \
+					--user_pass=$WP_PASSWORD \
 					--path='/var/www/html' \
 					--allow-root
 	
@@ -61,6 +70,8 @@ if ! wp user list --field=user_login  --path='/var/www/html' --allow-root | grep
 else
 	print_red "user $WP_USER already exists"
 fi
+
+sleep 1
 
 # launch php-pfm
 /usr/sbin/php-fpm82 -F
