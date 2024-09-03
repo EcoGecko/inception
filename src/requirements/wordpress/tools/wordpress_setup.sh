f65d6b5449eb9f23a2dd25cd3a413929dc48e2a6
Helder Mauricio da Silva Cabo <heda-sil@student.42porto.com>
2024-09-03 16:18:00 +0100

    cleaned script for wordpress

--- src/requirements/wordpress/tools/wordpress_setup.sh
+++ src/requirements/wordpress/tools/wordpress_setup.sh
@@ -2,36 +2,6 @@
 
 wp core download --path='/var/www/html' --allow-root
  
-# SQL_DATABASE=mariadb
-# SQL_HOST=mariadb:3306
-# SQL_USER=heda-sil
-# SQL_PASSWORD=0123456789
-# SQL_ROOT_PASSWORD=9876543210
-
-# # Wordpress
-# WP_URL=heda-sil.42.fr
-# WP_TITLE=no-title-page
-# WP_USER=heda-sil
-# WP_EMAIL=heda-sil@example.com
-# WP_PASSWORD=0123456789
-# WP_ADMIN=admin
-# WP_ADMIN_EMAIL=admin@example.com
-# WP_ADMIN_PASSWORD=9876543210
-
-# wp config create --dbname=$SQL_DATABASE \
-# 	--dbuser=$WP_USER \
-# 	--dbpass=$WP_PASSWORD \
-#  	--dbhost=$SQL_HOST \
-#  	--path='/var/www/html' \
-#  	--allow-root
-
-# wp core config --dbname=$SQL_DATABASE \
-#  	 --dbuser=$WP_USER \
-#  	 --dbpass=$WP_PASSWORD \
-#  	--dbhost=$SQL_HOST \
-#  	--path='/var/www/html' \
-#  	--allow-root
-
 if ! wp core is-installed --path='/var/www/html' --allow-root;
 	then
 		  wp core install --url=$WP_URL \
